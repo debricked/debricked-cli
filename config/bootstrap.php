e8832469b0409bc5b3c9aaa43b75c12699c76968
Gustav Svensson <gustav.svensson@debricked.com>
2019-10-13 13:58:59 +0200

    feature-b(921-select-branch-in-repository): Fixed comments

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -6,12 +6,12 @@ require dirname(__DIR__).'/vendor/autoload.php';
 
 // Load cached env vars if the .env.local.php file exists
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
-if (is_array($env = @include dirname(__DIR__).'/../.env.local.php')) {
+if (is_array($env = @include dirname(__DIR__).'/.env.local.php')) {
     $_ENV += $env;
 } elseif (!class_exists(Dotenv::class)) {
     throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
 } else {
-    $path = dirname(__DIR__).'/../.env';
+    $path = dirname(__DIR__).'/.env';
     $dotenv = new Dotenv(false);
 
     // load all the .env files
