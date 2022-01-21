366d5ae0a7bc93ef4fc428913b1f76d4e66d6c92
gustav <gustav.svensson@debricked.com>
2022-01-21 11:20:42 +0100

    Fixed code style

--- src/Command/CheckScanCommand.php
+++ src/Command/CheckScanCommand.php
@@ -13,8 +13,8 @@ namespace App\Command;
 use Debricked\Shared\API\API;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\HttpFoundation\Request;
@@ -206,8 +206,7 @@ class CheckScanCommand extends Command
                     break;
                 }
 
-                if (isset($status['progress']))
-                {
+                if (isset($status['progress'])) {
                     $progressBar->setProgress($status['progress']);
                 }
 
